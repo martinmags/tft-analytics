101a44e432444d8352422ff669d9199160f3705b
momongah <kzsidiqi@ucsd.edu>
2020-06-23 23:35:50 -0700

    my verion of redirect and fetching from flask endpoints

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useForm, Controller } from 'react-hook-form';
 import {  Button, Grid, TextField, MenuItem, FormControl, InputLabel, Select } from '@material-ui/core'; 
 import Alert from '@material-ui/lab/Alert';
 import SearchIcon from '@material-ui/icons/Search';
+import { Redirect } from 'react-router-dom';
 
 
 function Header() {
   const regions = ['na1','br1','oc1','la1','la2','jp1','kr','eun1','euw1','tr1','ru']
 
-  const { setValue, register, handleSubmit, errors, control } = useForm()
+  const { setValue, register, handleSubmit, errors, control } = useForm();
+  
+  const [toSummonerPage, setToSummonerPage] = useState(false);
 
+  // var redirectPath = "/SummonerPage";
+  let [redirPath, setPath] = useState("/summonerinfo");
+  
   const onSubmit = (data, e) => {
     console.log(data);
-    alert(JSON.stringify(data));
+    let j = JSON.stringify(data);
+    alert(j);
+        
+    setTimeout(() => setToSummonerPage(true), 0);
+    setTimeout(() => setPath(redirPath = "/summonerinfo"), 0);
+    setTimeout(() => setPath(redirPath + "/" + data.summoner + "/" + data.server), 1);
   };
 
   const handleChange = (event) => {
@@ -22,6 +33,9 @@ function Header() {
 
   return (
     <div>
+      {console.log(redirPath)}
+      {toSummonerPage ? <Redirect to={redirPath} /> : setTimeout(() => setToSummonerPage(false), 0)}
+      
       <Grid container direction="column" justify="center" alignItems="center" spacing={2}>
         <Grid item xs={12}>
           <h1>Welcome to TFT-Analytics</h1>
@@ -33,13 +47,13 @@ function Header() {
               as={
                 <FormControl variant="filled" size="small">
                   <InputLabel shrink>Region</InputLabel>
-                  <Select defaultValue="na1" name="region" onChange={handleChange}>
+                  <Select defaultValue="na1" name="server" onChange={handleChange}>
                     {regions.map((region) => <MenuItem key={region} value={region}>{region}</MenuItem>)}
                   </Select>
                 </FormControl>
               }
               control={control}
-              name="region"
+              name="server"
               defaultValue="na1"
             />              
             <TextField   
@@ -47,7 +61,7 @@ function Header() {
               inputRef={register({ required: true })}
               label="Summoner Name"
               size="small"
-              name="summonername"
+              name="summoner"
             />
 
             <Button type="submit" size="small" variant="contained" color="primary"   startIcon={<SearchIcon /> }   >Search</Button>
--- frontend/src/templates/SummonerPage.js
+++ frontend/src/templates/SummonerPage.js
@@ -4,8 +4,13 @@ import {  Grid, Box  } from '@material-ui/core';
 import SummonerNameCard from '../components/SummonerNameCard';
 import SummonerRankCard from '../components/SummonerRankCard';
 import MatchHistoryTabSelector  from '../components/MatchHistoryTabSelector';
+import {
+  useParams
+} from "react-router-dom";
+
 
 import MatchSummaryCard from '../components/MatchSummaryCard';
+import { useState } from 'react';
 const useStyles =  makeStyles((theme) =>({
   root: {
     flexGrow: 1,
@@ -26,18 +31,47 @@ const useStyles =  makeStyles((theme) =>({
 }));
  
 
-function SummonerPage() {
+function SummonerPage(props) {
   const classes = useStyles();
- 
+  let { summoner, server} = useParams();
+  console.log(summoner, server);
+  
+  const [name, setName] = useState(summoner);
+  const [profileIconId, setIcon] = useState();
+    
+  setTimeout(() => {
+    let url = new URL('http://localhost:8000/api/summonerinfo');
+    url.search = new URLSearchParams([['summoner', summoner], ['server', server]]).toString();
+    console.log(url);
+    
+    const f = fetch(url);
+
+    const p = f.then(r => r.json());
+
+    console.log(p);
+
+    p.then((data) => {
+      console.log(data);
+      setTimeout(() => setName(data.name), 0);
+      console.log(name);
+      setTimeout(() => setIcon(data.profileIconId), 0);
+      console.log(profileIconId);
+    })}, 0
+  );
+    
   return (  
     <Box  className={ classes.grey}>
+      {console.log( summoner )}
+      {}
+      <h1>Summoner: { summoner } </h1>
+      
         <Box mx={{  xs:2, md:16}} >
             <Grid   container   spacing={0} direction="row"   >
                 <Grid item xs={12} md={6}>
                     <SummonerNameCard 
-                        name = "BabyGerber"
+                        name = {name}
                         region = "NA"
-                        icon = "http://ddragon.leagueoflegends.com/cdn/10.12.1/img/profileicon/588.png"
+                        icon = {`http://ddragon.leagueoflegends.com/cdn/10.12.1/img/profileicon/${profileIconId}.png`}
                     >
                     </SummonerNameCard>
                 </Grid>          
