77d74a5fdb6a1439a050ace22a9e13e13d320291
martinmags <martinremags@gmail.com>
2020-06-25 22:51:27 -0700

    removed react context provider, ended up being unnecessary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
-import React, { useState, useMemo } from 'react';
+import React from 'react';
 import './App.css';
 import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { createMuiTheme, ThemeProvider , responsiveFontSizes } from '@material-ui/core/styles';
-import { SummonerContext } from './SummonerContext';
 /* Static Components */
 import Header from './components/Header'
 /* Dynamic Components */
@@ -13,21 +12,17 @@ import SummonerPage from './templates/SummonerPage'
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
 function App() {
-  const [summonerInfo, setSummonerInfo] = useState('')
-  const providerValue = useMemo(()=> ({summonerInfo, setSummonerInfo}), [summonerInfo, setSummonerInfo])
 
   return (
     <div className="App">
       <CssBaseline/>
       <ThemeProvider theme={theme}>
       <Router>
-        <SummonerContext.Provider value={providerValue}> {/* Allows access of Summoner info to any children components */}
           <Header/>
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/summonerstats/:region/:summonername" component={SummonerPage} />
           </Switch>
-        </SummonerContext.Provider>
       </Router>
       </ThemeProvider>
     </div>
